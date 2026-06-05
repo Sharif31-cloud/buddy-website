e089f39a8226553d91b95c9562d91aa3aeb3135f
Sharif <semasharks31@gmail.com>
2026-06-05 17:25:42 +0300

    updated version

--- api/send-telegram.js
+++ api/send-telegram.js
@@ -1,5 +1,31 @@
 export default function handler(req, res) {
+
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://sharif31-cloud.github.io"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  if (req.method === "POST") {
+    return res.status(200).json({
+      success: true
+    });
+  }
+
   return res.status(200).json({
-    version: "test123"
+    status: "API working"
   });
 }
